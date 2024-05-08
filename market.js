09e0081f2dedc45cb99785cbf5da80b650db90ce
nctrsiegeubsessed <163823373+nctrsiegeubsessed@users.noreply.github.com>
2024-05-08 09:43:32 +0300

    jscons

--- market.js
+++ market.js
@@ -1,25 +1,23 @@
-console.log('Перевірка підключеного файлу скриптів market.js')
+//console.log('Перевірка підключеного файлу скриптів market.js')
 
-let itemsDiv = document.getElementById("items");
 
-if (itemsDiv) {
-    
-} else {
-    console.log('Блок товару не знайдено')
-}
 let itemsArray = [
     'Газонокосарка 43',
     'Електричний тример 110',
     'Електрична газонокосарка 32',
     'Акамуляторний оприскувач 12 N',
-    'Газонокосарка 430',
-    'Електричний тример 130',
-    'Електрична газонокосарка 320',
-    'Акумуляторний оприскувач 12 E',
+    
 ]
-itemsArray.sort().forEach((item) => {
-    console.log( item)
-})
+let itemsDiv = document.getElementById("items");
+
+if (itemsDiv) {
+    itemsArray.forEach((item) => {
+       itemsDiv.innerHTML += '<div class = "item">${item}</div>'
+    })
+} else {
+    console.log('Блок товару не знайдено')
+}
+
 
 // itemsArray = itemsArray.sort()
 
